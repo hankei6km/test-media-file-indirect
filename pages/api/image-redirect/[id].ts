26854fb8bfe49cce6c4153e3419ac29ce0413082
hankei6km <gh-hankei6km@outlook.jp>
2021-01-31 19:05:27 +0900

    Set cors fields to response header
    It allows image files to be fetched from other domains.

--- pages/api/image-redirect/[id].ts
+++ pages/api/image-redirect/[id].ts
@@ -1,13 +1,48 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { mediaUrl } from '../../../lib/media';
 
+// https://vercel.com/knowledge/how-to-enable-cors?query=access-control-allow-origin
+const allowCors = (
+  fn: (req: NextApiRequest, res: NextApiResponse<any>) => Promise<void>
+) => async (req: NextApiRequest, res: NextApiResponse) => {
+  // res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    // 'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+    'GET,OPTIONS'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const m = await mediaUrl(req?.query);
-    if (m) {
-      res.redirect(307, m);
+    if (req.method === 'GET') {
+      const m = await mediaUrl(req?.query);
+      if (m) {
+        // https://vercel.com/knowledge/how-to-enable-cors?query=access-control-allow-origin
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+        res.setHeader(
+          'Access-Control-Allow-Headers',
+          'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+        );
+        res.redirect(307, m);
+      } else {
+        res.status(404);
+      }
     } else {
-      res.status(404);
+      res.status(500).json({ statusCode: 500, message: 'bad request' });
     }
     res.end();
   } catch (err) {
@@ -15,4 +50,4 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   }
 };
 
-export default handler;
+export default allowCors(handler);
