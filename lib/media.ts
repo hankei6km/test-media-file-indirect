e8790404e1e5f21adeb4f199cc2aa48285caabbe
hankei6km <gh-hankei6km@outlook.jp>
2021-02-19 08:41:46 +0900

    Lookup filename only in json file

--- lib/media.ts
+++ lib/media.ts
@@ -15,42 +15,6 @@ export type Media = {
   height: number;
 };
 
-export async function getMediaItems(files: string[]): Promise<Media[]> {
-  if (process.env.GET_API_KEY === undefined) {
-    console.error('$GET_API_KEY not defined');
-    throw new Error('env vars error: see server log');
-  } else if (process.env.API_BASE_URL === undefined) {
-    console.error('$API_BASE_URL not defined');
-    throw new Error('env vars error: see server log');
-  } else {
-    const q = new URLSearchParams('');
-    q.append('limit', '1000');
-    q.append('fields', 'media');
-    if (files.length > 0) {
-      q.append('filters', `media[equals]${files.join('[or]media[equals]')}`);
-    }
-    const r = await fetch(`${process.env.API_BASE_URL}media?${q.toString()}`, {
-      method: 'GET',
-      headers: { 'X-API-KEY': process.env.GET_API_KEY || '' }
-    }).catch((err) => {
-      throw err.name;
-    });
-    if (r.ok) {
-      const items = (await r.json()).contents.map((content: Content) => {
-        const fileName = basename(new URL(content.media.url).pathname);
-        return {
-          fileName: fileName,
-          rawUrl: content.media.url,
-          width: content.media.width,
-          height: content.media.height
-        };
-      });
-      return items;
-    }
-  }
-  return [];
-}
-
 export async function getMediaItemsFromFile(files: string[]): Promise<Media[]> {
   const items = (apiRes.contents as Content[]).map(({ media }) => {
     const fileName = basename(new URL(media.url).pathname);
@@ -69,6 +33,48 @@ export async function getMediaItemsFromFile(files: string[]): Promise<Media[]> {
   return items;
 }
 
+export async function getMediaItems(files: string[]): Promise<Media[]> {
+  return await getMediaItemsFromFile(files);
+
+  // 2021-02-17 のメディアファイル管理の仕様変更で
+  // filters による画像フィールドのファイル名は使えなかったもよう.
+  // よって事前にダウンロードしておいたリストので対応.
+  //
+  // if (process.env.GET_API_KEY === undefined) {
+  //   console.error('$GET_API_KEY not defined');
+  //   throw new Error('env vars error: see server log');
+  // } else if (process.env.API_BASE_URL === undefined) {
+  //   console.error('$API_BASE_URL not defined');
+  //   throw new Error('env vars error: see server log');
+  // } else {
+  //   const q = new URLSearchParams('');
+  //   q.append('limit', '1000');
+  //   q.append('fields', 'media');
+  //   if (files.length > 0) {
+  //     q.append('filters', `media[equals]${files.join('[or]media[equals]')}`);
+  //   }
+  //   const r = await fetch(`${process.env.API_BASE_URL}media?${q.toString()}`, {
+  //     method: 'GET',
+  //     headers: { 'X-API-KEY': process.env.GET_API_KEY || '' }
+  //   }).catch((err) => {
+  //     throw err.name;
+  //   });
+  //   if (r.ok) {
+  //     const items = (await r.json()).contents.map((content: Content) => {
+  //       const fileName = basename(new URL(content.media.url).pathname);
+  //       return {
+  //         fileName: fileName,
+  //         rawUrl: content.media.url,
+  //         width: content.media.width,
+  //         height: content.media.height
+  //       };
+  //     });
+  //     return items;
+  //   }
+  // }
+  // return [];
+}
+
 export async function mediaUrl(
   reqQuery: Record<string, string | string[]>
 ): Promise<string> {
@@ -85,8 +91,9 @@ export async function mediaUrl(
   if (items && items.length > 0) {
     console.log(`GET MEDIA ITEMS:HIT : ${reqQuery.id}`);
   } else {
-    console.log(`GET MEDIA ITEMS:FALLBACK FROM NETWORK : ${reqQuery.id}`);
-    items = await getMediaItems(files);
+    // console.log(`GET MEDIA ITEMS:FALLBACK FROM NETWORK : ${reqQuery.id}`);
+    // items = await getMediaItems(files);
+    console.log(`GET MEDIA ITEMS:NOT FOUND : ${reqQuery.id}`);
   }
   const et = Date.now();
   console.log(`GET MEDIA ITEMS:START: ${st}`);
